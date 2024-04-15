8f896e8b2574a6df16203848e7ba8b52e8f964a5
peko <yoyo.mepa@gmail.com>
2024-04-15 16:24:23 +0200

    tailwind fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,22 @@
 import Link from "next/link";
 
 export default function HomePage() {
+
+  const mocks = [
+    'https://mhcibtcjhobsbydm.public.blob.vercel-storage.com/1604392475636-um9zickrm1A48SZmY3XuvnAml698Jd.jpeg',
+    'https://mhcibtcjhobsbydm.public.blob.vercel-storage.com/WhatsApp%20Image%202024-04-11%20at%2013.20.24-AMynBg7n8A9KRJjL6nm8RFHcrVI0n9.jpeg',
+    'https://mhcibtcjhobsbydm.public.blob.vercel-storage.com/peakpx-8k35q06rI5SVg3SKj55Pd6mVY3udkD.jpg'
+  ];
+
+  const mockImages = mocks.map((mock, index) => (
+    <div key={index} className="w-48">
+      <img src={mock} alt="mock" className="w-full" />
+    </div>
+  ));
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-    hello les des chargames
+    <main className="text-white h-screen main">
+      <div className="flex flex-wrap w-full gap-4">{mockImages}</div>
     </main>
   );
 }
